2c0054ef3fc5b83eeac0c2a5bd41d500af6ae86c
shcherbakov.vas <Vasily.Shcherbakov@infotecs.ru>
2022-11-28 08:21:38 +0300

    fix

--- migrations/01_init_up.sql
+++ migrations/01_init_up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE default.event_log (message String) 
-ENGINE = Kafka('host.docker.internal:9092', 'Nsms', 'group1', 'LineAsString');
+ENGINE = Kafka('host.docker.internal:9092', 'Nsms', 'clickhouse-group', 'LineAsString');
 
 CREATE MATERIALIZED VIEW default.event_log_view
 ENGINE = Memory
